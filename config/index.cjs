97664fa6674b0ad07d141570cba03c753b6aacbb
Hsusalome <111xuxuewen@sina.com>
2024-04-01 16:55:36 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: 'biu～',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'o8C-06m0u_1acVzb_hFlj0bOO1y4	',
+      id: 'o8C-06m0u_1acVzb_hFlj0bOO1y4',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'Iqazx1enaBRoYH6fHV7qJBFX7gIgBH8vbsCLJdhIlJw',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
